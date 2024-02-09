6af109b0d1c878498325b05e0b7ce81c33f7cc0c
anantamishra <mishraananta2017@gmail.com>
2024-02-09 11:43:32 +0545

    useEffect hook

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,24 @@
+'use client'
+import { useEffect } from "react"
 
 export default function AboutPage() {
+    // use of useEffect
+
+    useEffect(() => {
+        console.log("About Page")
+    })
+
+    useEffect(() => {
+        console.log("About 1 Page")
+    }, [])
+
+    useEffect((a) => {
+        console.log("About 12 Page")
+    }, [a])
+
+
+
+
     return (
         <div>
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,53 +1,66 @@
 'use client'
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function Home() {
-  const [count, setCount] = useState(0);
-  const [fullName, setFullName] = useState("");
+export default function HomePage() {
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+  const [value3, setValue3] = useState("");
+  const [value4, setValue4] = useState("");
+  const [additionResult, setAdditionResult] = useState("");
+  const [subtractionResult, setSubtractionResult] = useState("");
 
-  const updateValue = () => {
-    setCount(count + 1);
-  };
-  const reset = () => {
-    setCount(0);
-  };
-
-  const updateName = (e) => {
+  const handleAddition = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const firstname = formData.get("firstname");
-    const lastname = formData.get("lastname");
-    setFullName(firstname + " " + lastname);
+    setValue1(formData.get("val1"));
+    setValue2(formData.get("val2"));
   };
 
+  function handleSubtraction(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    setValue3(formData.get("val3"));
+    setValue4(formData.get("val4"));
+  };
+
+
+  useEffect(() => {
+    if (value1 !== "" && value2 !== "") {
+      setSubtractionResult("")
+      setAdditionResult("Addition Result is: " + (parseInt(value1) + parseInt(value2)));
+    }
+  }, [value1, value2]);
+
+  useEffect(() => {
+    if (value3 !== "" && value4 !== "") {
+      setAdditionResult("")
+      setSubtractionResult("Subtraction Result is: " + (parseInt(value3) - parseInt(value4)));
+    }
+  }, [value3, value4]);
   return (
-    <div>
-      <div className="flex flex-col min-h-screen w-full justify-center items-center gap-5">
-        <div className="text-2xl font-semibold text-blue-500">{count}</div>
-        <button onClick={updateValue} className="bg-blue-500 px-5 py-2 text-white hover:bg-blue-400">
-          Increment
-        </button>
-        <button onClick={reset} className="bg-blue-500 px-5 py-2 text-white hover:bg-blue-400">
-          Reset
-        </button>
-      </div>
-      <div className="flex min-h-screen bg-black w-full justify-center items-center flex-col gap-5">
-        <h1 className="text-white text-2xl">Input field Form for React</h1>
-        <form className="flex flex-col gap-3" onSubmit={updateName}>
-          <div className="flex flex-col gap-3">
-            <p className="text-white text-xl">Enter your name</p>
-            <input type="text" name="firstname" placeholder="Enter your name" className="px-6 py-2" />
+    <div className="flex flex-col w-full min-h-screen justify-center items-center gap-5">
+      <div>
+        <form className="flex flex-col w-full items-center gap-3" onSubmit={handleAddition}>
+          <h1 className="text-xl font-semibold">Addition Form</h1>
+          <div className="flex gap-3">
+            <input type="number" name="val1" className="border-2 border-black px-3 py-2 rounded focus:outline-none focus:border-blue-500" />
+            <input type="number" name="val2" className="border-2 border-black px-3 py-2 rounded focus:outline-none focus:border-blue-500" />
+            <button type="submit" className="bg-blue-500 px-5 py-2 rounded text-white hover:bg-blue-300">Calculate</button>
           </div>
-          <div className="flex flex-col gap-3">
-            <p className="text-white text-xl">Enter your name</p>
-            <input type="text" name="lastname" placeholder="Enter your name" className="px-6 py-2" />
-          </div>
-          <button type="submit" className="bg-white px-6 py-2 font-semibold hover:bg-white/90">
-            Submit
-          </button>
         </form>
-        <p className="text-white text-2xl">{fullName}</p>
       </div>
+      <div>
+        <form className="flex flex-col w-full items-center gap-3" onSubmit={handleSubtraction}>
+          <h1 className="text-xl font-semibold">Subtraction Form</h1>
+          <div className="flex gap-3">
+            <input type="number" name="val3" className="border-2 border-black px-3 py-2 rounded focus:outline-none focus:border-blue-500" />
+            <input type="number" name="val4" className="border-2 border-black px-3 py-2 rounded focus:outline-none focus:border-blue-500" />
+            <button type="submit" className="bg-blue-500 px-5 py-2 rounded text-white hover:bg-blue-300">Calculate</button>
+          </div>
+        </form>
+      </div>
+      <p className="text-xl font-semibold">{additionResult}</p>
+      <p className="text-xl font-semibold">{subtractionResult}</p>
     </div>
   );
 }
